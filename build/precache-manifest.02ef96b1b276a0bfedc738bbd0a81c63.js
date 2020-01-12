ea1163219a770c41df63d706279ca07bac64d425
Laura Tikker <tikker.laura@gmail.com>
2020-01-12 12:30:52 +0200

    frontend build files added

---
+++ build/precache-manifest.02ef96b1b276a0bfedc738bbd0a81c63.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6434774484ea5a2e19967d301c7a1fe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "617a15e31213bb1500d0",
+    "url": "/static/css/main.252dd0d5.chunk.css"
+  },
+  {
+    "revision": "4efaef099602235819a0",
+    "url": "/static/js/2.e5533f34.chunk.js"
+  },
+  {
+    "revision": "617a15e31213bb1500d0",
+    "url": "/static/js/main.f3642725.chunk.js"
+  },
+  {
+    "revision": "46e8288a0c870cee436a",
+    "url": "/static/js/runtime-main.356d0999.js"
+  }
+]);
